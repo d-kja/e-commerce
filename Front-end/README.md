63147e79586d28abee040817cf9a1de1323c076a
d-kja <89911360+d-kja@users.noreply.github.com>
2023-08-28 16:56:48 -0300

    initial setup

---
+++ Front-end/README.md
@@ -0,0 +1,16 @@
+# E-commerce
+
+A dynamic e-commerce layout as a product
+
+## Intro
+
+todo...
+
+## TO-DO
+
+- [ ] Home page
+- [ ] Products page
+- [ ] Product page
+- [ ] Scheduling page
+- [ ] Checkout page
+- [ ] About page
---
+++ Front-end/tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: ['./src/**/*.{ts,tsx,mdx}'],
+  theme: {
+    extend: {
+      backgroundImage: {},
+    },
+  },
+  plugins: [],
+}
+export default config
