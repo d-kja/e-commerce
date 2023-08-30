5d8b0b1c146fcec12259ae66630585a5c2c30bad
d-kja <89911360+d-kja@users.noreply.github.com>
2023-08-30 11:39:48 -0300

    finish storybook setup

---
+++ apps/docs/src/stories/button.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Button } from '@sw-ec/ui'
+
+export default {
+  title: 'components/button',
+  component: Button,
+} as Meta
+
+export const Default: StoryObj = {}
